7a3ed39a6df6393f3174a9b4e6ccbcd58877f633
VitalJeevanjot <jeevanjotsinghvital@gmail.com>
2025-05-14 03:27:17 +0530

    Initial Commit

---
+++ webkitViewer-tutorial/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  webkitViewer-tutorial
+//
+//  Created by Jeevanjot Singh on 14/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ webkitViewer-tutorial/webkitViewer_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  webkitViewer_tutorialApp.swift
+//  webkitViewer-tutorial
+//
+//  Created by Jeevanjot Singh on 14/05/25.
+//
+
+import SwiftUI
+
+@main
+struct webkitViewer_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
